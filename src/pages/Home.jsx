002ff5224b95f3afddcc6f60b3433cae2eff5c4f
Licornhizix <licornhizix@gmail.com>
2022-11-13 16:06:14 +0100

    création de la base react de l'application

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Survey from '../pages/Survey';
+
+function Home() {
+
+    return(
+//alert('il est samedi');
+<Survey />
+    );
+
+}
+
+export default Home; 
---
+++ src/pages/Survey.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+function Survey() {
+    return (
+        <div>
+            <h1>Questionnaire 🧮</h1>
+        </div>
+    );
+}
+
+export default Survey;
